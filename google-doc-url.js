06914396396b6651a2ac16554d08b69bd45c1797
Carlos Tafur Porras <carlostafurporras@gmail.com>
2025-05-27 20:53:36 +0200

    Update google api key

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1wYJinC76uYABIsQjkzc5KYlV9xhoBL9qedotVh13NuA/edit?gid=0#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyA2uYV8sN9XH3MPvpogSwx0zTMkzmkPHQU';
